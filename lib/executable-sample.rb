d83dce6f39b99b953b74b686228d0b28836a9f25
sakai shunsuke <sakai@ans-web.co.jp>
2013-08-30 17:59:18 +0900

    add class

--- lib/executable-sample.rb
+++ lib/executable-sample.rb
@@ -2,6 +2,16 @@ require "executable-sample/version"
 
 module Executable
   module Sample
-    # Your code goes here...
+    class Main
+      def initialize(name)
+        @name = name
+      end
+
+      attr_reader :name
+
+      def say(message)
+        puts "#{message}, #{name}"
+      end
+    end
   end
 end
